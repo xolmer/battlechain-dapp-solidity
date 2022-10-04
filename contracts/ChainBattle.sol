2ef9c0619113576795f1cca94d02872cabfa9bc2
Kaveh Aidivandi <aidivandi.dev@gmail.com>
2022-10-04 17:31:54 +0200

    NFT Contract

--- contracts/ChainBattle.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
-
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
-import "@openzeppelin/contracts/utils/Base64.sol";
-
-contract ChainBattle {}
